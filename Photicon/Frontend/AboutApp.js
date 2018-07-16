779401af7832997d08f45cc98326698c2a765bfc
André Santos <santos.as96@hotmail.com>
2018-07-16 15:18:21 +0100

    Api fixed append userContainer null

--- Photicon/Frontend/AboutApp.js
+++ Photicon/Frontend/AboutApp.js
@@ -1,5 +1,4 @@
 ﻿$(function () {
-    debugger;
     adqInterf();
     usersStage();
 });
@@ -17,7 +16,6 @@ function usersStage() {
         });
 }
 function mostraUsers(users) {
-    debugger;
     for (var i = 0; i < users.length; i++) {
         var header = document.createElement('label');
         header.textContent = users[i].UserName;
